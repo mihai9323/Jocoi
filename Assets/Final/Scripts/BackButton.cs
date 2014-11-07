df88e9a4242a77c3131694a3b9af376ee7fbd126
Camilla <camilla.g.jakobsen@hotmail.com>
2014-11-07 20:53:13 +0100

    Back Button

---
+++ Assets/Final/Scripts/BackButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButton : MonoBehaviour {
+
+	public float timeUntilBack;
+	public int sceneToLoad;
+	public Transform button;
+	private bool canPress;
+	private float xButton;
+	private float yButton;
+
+	void Start () {
+		xButton = ((Screen.width/4)*3);
+		yButton = ((Screen.height/4)*3);
+		canPress = false;
+		StartCoroutine("enableButton", timeUntilBack);
+	}
+
+	void OnGUI(){
+		if(canPress){
+			if(GUI.Button(new Rect(xButton, yButton, 100f, 50f), "Back to normal")){
+				Application.LoadLevel(sceneToLoad);
+			}
+		}
+	}
+
+	private IEnumerator enableButton(float timeUntilBack){
+		yield return new WaitForSeconds(timeUntilBack);
+		canPress = true;
+		Debug.Log("button enabled");
+	}
+}
