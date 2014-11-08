3172c261e00aaf0477d4c3862d0feb89285a59c8
cristianton2009 <cristianton2009@gmail.com>
2014-11-08 18:26:51 +0100

    Awesome changes by Chris
    you won't believe it

---
+++ Assets/TestFolders/Cristian/sun/starGrow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class starGrow : MonoBehaviour {
+	public float amplitude;
+	Vector3 scale;
+	float r;
+	void Start()
+	{ 
+
+		scale = transform.localScale;
+		r = Random.Range(11.0f, 2.0f);
+	}
+
+	void Update ()
+
+
+	{
+		float t = Time.deltaTime;
+		transform.localScale = new Vector3 ((transform.localScale.x - (Mathf.Sin(Time.time*r)*amplitude)), (transform.localScale.y - (Mathf.Sin(Time.time*r)*amplitude)), (transform.localScale.z - (Mathf.Sin(Time.time*r)*amplitude)));
+
+
+		//transform.localScale += Vector3.Lerp(scale,  (transform.localScale * (Mathf.Sin(Time.time)*amplitude)), Time.deltaTime);
+
+	
+
+
+
+	}
+}
