2b08fd790e3bad2ae0b859ac3d41772f6473248e
Camilla <camilla.g.jakobsen@hotmail.com>
2014-11-08 17:05:25 +0100

    WalkToRiver done, fix to CameraShake

--- Assets/Final/Scripts/Camera/CameraShake.cs
+++ Assets/Final/Scripts/Camera/CameraShake.cs
@@ -8,21 +8,7 @@ public class CameraShake : MonoBehaviour {
 	public AnimationCurve shake;
 
 	void Update () {
-		if (Input.GetMouseButton (0)) {
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			if (Physics.Raycast (ray, out hit)) {
-				if (hit.transform.name == "Cube") {
-					Debug.Log ("My object is clicked by mouse");
-					Vector3 localPosition = transform.localPosition;
-					localPosition.y = shake.Evaluate (timer);
-					transform.localPosition = localPosition;
-					// Increase the timer by the time since last frame
-					timer += Time.deltaTime;
-					Debug.Log (timer);
-					}
-				}
-		}
+
 	}
 
 	void StartShake(){
--- Assets/Final/Scripts/WalkToRiver.cs
+++ Assets/Final/Scripts/WalkToRiver.cs
@@ -3,49 +3,94 @@ using System.Collections;
 
 public class WalkToRiver : MonoBehaviour {
 
-	public float movementSpeed;
+	public float movementSpeed = 2f;
 	public float maxZoom = 20f;
-	private float newZoom;
-	private float startZoom;
-	private float moveDistance;
 
 	public AnimationCurve headMovement;
-	public float animationCurveTime;
-
 	public AudioClip sound;
 	public int endScene;
 	public Camera camera;
+	private float timer = 0;
 
-	public float eyelidLength;
+	private float forwardZoom;
+	private float backwardZoom;
+	private float startZoom;
+
+	private float eyelidDistance;
+	private float eyelidLength;
+	private float eyelidSteps;
+	private float moveDistance;
+	public GameObject eyelidTop;
+	public GameObject eyelidBottom;
+	private Vector3 eyelidTopPos;
+	private Vector3 eyelidBottomPos;
+	private Vector3 eyelidMove;
 
 	void Start () {
+		eyelidTopPos = eyelidTop.transform.position;
+		eyelidBottomPos = eyelidBottom.transform.position;
 		startZoom = camera.fieldOfView;
+		eyelidDistance = eyelidTop.transform.position.y - eyelidBottom.transform.position.y;
+		eyelidLength = eyelidDistance / 2;
 		MoveDistance();
+		eyelidMove.Set(0f, moveDistance,0f);
 	}
 	
 	void Update () {
 		//   wolf/sheep/lamb pressed
-		if(true){
-			//not reached maxZoom
-			if(camera.fieldOfView > maxZoom){
-				AudioSource.PlayClipAtPoint(sound, camera.transform.position);
-				//eyelid thingy goes here
+		if (Input.GetMouseButton (0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit)) {
+				if (hit.transform.name == "Cube") {
+					Debug.Log ("My object is clicked by mouse");
+					//not reached maxZoom
+					if (camera.fieldOfView > maxZoom) {
+						//eyelid
+						eyelidTop.transform.position = Vector3.Lerp (eyelidTop.transform.position, (eyelidTop.transform.position - eyelidMove), 1 * Time.deltaTime);
+						eyelidBottom.transform.position = Vector3.Lerp (eyelidBottom.transform.position, (eyelidBottom.transform.position + eyelidMove), 1 * Time.deltaTime);
+						//zoom
+						forwardZoom = camera.fieldOfView - movementSpeed;
+						camera.fieldOfView = Mathf.Lerp (camera.fieldOfView, forwardZoom, 1 * Time.deltaTime);
 
-				//zoom
-				newZoom = camera.fieldOfView - movementSpeed;
-				camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, newZoom, (animationCurveTime * Time.deltaTime));
+						//head
+						Vector3 localPosition = camera.transform.localPosition;
+						localPosition.y = headMovement.Evaluate (timer);
+						camera.transform.localPosition = localPosition;
+						// Increase the timer by the time since last frame
+						timer += Time.deltaTime;
+					}
+					//reached maxZoom
+					else {
+						Application.LoadLevel (endScene);
+					}
+				}
 			}
-			//reached maxZoom
-			else{
-				Application.LoadLevel(endScene);
+		} else{
+			if(camera.fieldOfView < startZoom){
+				//eyelids
+				eyelidTop.transform.position = Vector3.Lerp (eyelidTop.transform.position, (eyelidTop.transform.position + eyelidMove), 1 * Time.deltaTime);
+				eyelidBottom.transform.position = Vector3.Lerp (eyelidBottom.transform.position, (eyelidBottom.transform.position - eyelidMove), 1 * Time.deltaTime);
+				//Zoom
+				backwardZoom = camera.fieldOfView + movementSpeed;
+				camera.fieldOfView = Mathf.Lerp (camera.fieldOfView, backwardZoom, 1 * Time.deltaTime);
+				//head
+				Vector3 localPosition = camera.transform.localPosition;
+				localPosition.y = headMovement.Evaluate (timer);
+				camera.transform.localPosition = localPosition;
+				// Increase the timer by the time since last frame
+				timer += Time.deltaTime;
 			}
 		}
 	}
 
 	void MoveDistance(){
-		float zoom = startZoom - maxZoom;
-		float parts = zoom/movementSpeed;
-		moveDistance = eyelidLength/parts;
+		float zoom = (startZoom - maxZoom);
+		Debug.Log ("zoom is " + zoom);
+		float parts = (zoom/movementSpeed);
+		Debug.Log ("parts is " + parts);
+		moveDistance = (eyelidLength/parts);
+		Debug.Log ("moveDistance is " + moveDistance);
 	}
 	
 }
