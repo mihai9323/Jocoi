47ce6e0426fb7e17182e10f97bf740f7f5519002
mihai9323 <mihai9323@gmail.com>
2014-11-11 20:26:58 +0200

    MemoryClean
    Made script that cleans the game data when entering the first stage

---
+++ Assets/Final/Scripts/MemoryClean.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryClean : MonoBehaviour {
+
+	private void Start(){
+		GameData.Memory = new System.Collections.Generic.List<Flower>();
+	}
+}
