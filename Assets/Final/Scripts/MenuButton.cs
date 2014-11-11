a5a36371849e159a03312f2584efafa4780e004a
Camilla <camilla.g.jakobsen@hotmail.com>
2014-11-11 19:40:12 +0100

    MenuButton script

---
+++ Assets/Final/Scripts/MenuButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour {
+
+	public Color initialColor;
+	public Color changedColor;
+	public string nextLevelName;
+	public float fadeOutTime;
+	public float fadeInTime;
+	public Color fadeColor;
+
+	void Start () {
+		renderer.material.color = initialColor;
+	}
+	void Update () {}
+
+	void OnMouseOver(){
+		renderer.material.color = changedColor;
+	}
+
+	void OnMouseExit (){
+		renderer.material.color = initialColor;
+	}
+
+	void OnMouseDown(){
+		AutoFade.LoadLevel (nextLevelName, fadeOutTime, fadeInTime, fadeColor);
+	}
+}
